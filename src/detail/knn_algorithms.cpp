ad9299a7e1c7e94398b64a0b5128ff723a387425
Pansysk75 <pansysk75@gmail.com>
2023-01-07 02:45:20 +0200

    Add # parts heuristic for knn_blas_in_parts, use in worker

--- src/detail/knn_algorithms.cpp
+++ src/detail/knn_algorithms.cpp
@@ -288,6 +288,17 @@ ResultPacket knn_blas_in_parts(const QueryPacket &query, const CorpusPacket &cor
     return res;
 }
 
+
+ResultPacket knn_blas_in_parts(const QueryPacket &query, const CorpusPacket &corpus, const size_t k_arg)
+{
+    // Determine part size to limit memory usage
+    const size_t part_bytes_lim = 500000;
+    const size_t part_doubles_lim = part_bytes_lim/sizeof(double);
+    const size_t part_lines_lim = std::max(part_doubles_lim/corpus.n_packet, 1ul);
+    const size_t n_parts = (query.m_packet + part_lines_lim - 1)/part_lines_lim;
+    return knn_blas_in_parts(query, corpus, k_arg, n_parts);
+}
+
 ResultPacket knn_dynamic(const QueryPacket &query, const CorpusPacket &corpus, size_t k_arg)
 {
 
--- src/detail/knn_algorithms.hpp
+++ src/detail/knn_algorithms.hpp
@@ -4,6 +4,7 @@ ResultPacket knn_blas(const QueryPacket &query,
                       const CorpusPacket &corpus, size_t k_arg);
 
 ResultPacket knn_blas_in_parts(const QueryPacket &query, const CorpusPacket &corpus, const size_t k_arg, const size_t parts);
+ResultPacket knn_blas_in_parts(const QueryPacket &query, const CorpusPacket &corpus, const size_t k_arg);
 
 ResultPacket knn_simple(const QueryPacket &query, const CorpusPacket &corpus, size_t k_arg);
 
