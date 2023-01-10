359b68660cf1ee5a200fecb0476fd89e84132d14
isidorostsa <tsa.isidoros@gmail.com>
2023-01-11 00:10:34 +0200

    default size=500mb

--- src/detail/globals.cpp
+++ src/detail/globals.cpp
@@ -2,7 +2,7 @@
 
 namespace globals
 {
-    size_t knn_part_bytes_limit = 500000;
+    size_t knn_part_bytes_limit = 500*1000*1000; // 500 megabytes
     bool pad = false;
     bool debug = false;
 
